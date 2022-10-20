b7d80b48bfe2bc0141342c5ba3411d973cdc1702
Sumayia Rashid <sumayiarashid@Sumayia.lan>
2022-10-19 23:12:12 -0400

    getting there

--- indent.cpp
+++ indent.cpp
@@ -9,11 +9,11 @@ This program countS the number of open and closed { } in it, and keepS track of 
 #include <iostream>
 #include <string>
 
+std::string removeLeadingSpaces(std::string line);
 int countChar(std::string line, char c);
 
 int countChar(std::string line, char c){
     int braces;
-
     for(int i = 0; i < line.length(); i++){
         if(line[i] == c){
             braces++;
@@ -22,10 +22,29 @@ int countChar(std::string line, char c){
     return braces;
 }
 
+std::string removeLeadingSpaces(std::string line){
+    std::string endResult = ""; //contains the string that will return unindented
+    bool match = true;
+    for (int i = 0; i < line.size(); i++){
+        if (!(match && isspace(line[i]))){
+            endResult += line[i]; //adds elements of the string line to the unindented string "endResult"
+            match = false;
+        }
+    }
+    return endResult;
+}
+
 int main(){
     std::string a;
-    char b;
-
-    std::cout << countChar(a,b) << std::endl;
+    int b = 0;
+    while (getline(std::cin, a)){
+        a = removeLeadingSpaces(a);
+        b -= countChar(a, '}');
+        for (int i = 0; i < b; i++){
+            std::cout << "\t"; 
+        }
+        std::cout << a << std::endl;
+        b += countChar(a, '{');
+    }
     return 0;
 }
